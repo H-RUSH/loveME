2884df20db3ad6c8a0c9f852f7f2f40e260d7f11
Himadry Mondol <himadrymondol81@gmail.com>
2026-08-15 02:36:57 +0600

    this is the function time

--- lecture43/script.js
+++ lecture43/script.js
@@ -1,32 +1,21 @@
-// for(let x=1;x<10;x++){
-//     console.log("hellow mitrrooo");
+// console.log(`hellow bonshi`);
+// function function1(){
+//     console.log(`Hellow bro`);
+//     console.log(`Hellow bro`);
+//     console.log(`Hellow bro`);
 // }
-// for(let y=10;y>0;y--){
-//     console.log(y);
-// }
-// for(let x=1;x<10;x++){
-//     if(x==5){
-//         break;
-//     }
-//     else{
-//         console.log(x)
-//     }
-// }
-// for(let x=1;x<10;x++){
-//     if(x==5){
-//         continue;
-//     }
-//     else{
-//         console.log(x)
+// function1();
+// let meye=`no`;
+// let another=``;
+// let cele=`please`;
+// console.log("mene bola no")
+// let ans = window.prompt(`mene bola no`);
+// if(ans=='please'){
+//     ans2= window.prompt(`mene bola no`);;
+//     if(ans=='please'){
+//         window.alert(`ok`);
 //     }
 // }
-// let x=10;
-// while(x>=1){
-//     console.log(x);
-//     x--;
+// else{
+//     window.alert(`getlost`);
 // }
-// let x=4;
-// do{
-//     console.log(x);
-//     x--;
-// }while(x>=1)
