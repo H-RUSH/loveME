380c7ae8fc067e7dabb13f020152f9aa3b1fe67a
Himadry Mondol <himadrymondol81@gmail.com>
2026-08-20 03:54:35 +0600

    this is some hardwere knowledge what i just spent my time on its though on my hard paper

---
+++ lecture42/script.js
@@ -0,0 +1,32 @@
+// for(let x=1;x<10;x++){
+//     console.log("hellow mitrrooo");
+// }
+// for(let y=10;y>0;y--){
+//     console.log(y);
+// }
+// for(let x=1;x<10;x++){
+//     if(x==5){
+//         break;
+//     }
+//     else{
+//         console.log(x)
+//     }
+// }
+// for(let x=1;x<10;x++){
+//     if(x==5){
+//         continue;
+//     }
+//     else{
+//         console.log(x)
+//     }
+// }
+// let x=10;
+// while(x>=1){
+//     console.log(x);
+//     x--;
+// }
+// let x=4;
+// do{
+//     console.log(x);
+//     x--;
+// }while(x>=1)
---
+++ lecture45/script.js
@@ -0,0 +1,1 @@
+let human=function();
